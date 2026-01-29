fd246b1d0c3c6f470de454f3247c062919136229
Melvin-M-George <melvinmgeorge1234@gmail.com>
2026-01-29 12:45:51 +0530

    user routes added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
-import {registerUser, loginUser, getUser, editUser, logout} from "../controllers/userController"
+import { registerUser,loginUser,getUser,editUser,logout } from "../controllers/userController";
 
-const router = express.Router();
+const router = express();
 
 router.post("/signup",registerUser);
+router.post("/login",loginUser);
+router.get("/user-details/:id",getUser);
+router.put("/edit-user/:id",editUser);
+router.post("/logout",logout);
+
+export default router;
